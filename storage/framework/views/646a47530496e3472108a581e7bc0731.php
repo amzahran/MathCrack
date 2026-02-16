8a0b0d3dc246b1a7785ad5b5ba017c214dc17f2d
Ahmed Zahran <ahmed@example.com>
2026-02-16 04:19:18 +0200

    ignore storage cache

--- storage/framework/views/646a47530496e3472108a581e7bc0731.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH /home/u121952710/domains/mathcrack.com/public_html/resources/views/vendor/mail/text/header.blade.php ENDPATH**/ ?>
--- storage/framework/views/efcfdd70cc3d8591e52a6ca0ceaa574f.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH /home/u121952710/domains/mathcrack.com/public_html/resources/views/vendor/mail/text/footer.blade.php ENDPATH**/ ?>
